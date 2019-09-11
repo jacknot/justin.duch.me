61d27a57ce4b226bc251332b1177e9d5e80b3b80
Justin Duch <justin@justinduch.com>
2019-09-11 16:30:24 +1000

    quote route

---
+++ src/routes/quote.js
@@ -0,0 +1,27 @@
+// Route to get a random quote from any article
+import scanPosts from './article/_posts.js';
+
+export function get(req, res) {
+	let tags = [];
+
+	scanPosts().then(posts => {
+		posts.forEach(post => {
+			tags = tags.concat(
+				// Regex to match all p tags
+				post.html.match(/<p>(.*?)<\/p>/g).map(
+					// Remove <p> tags but keep other tags inside
+					// E.g '<p><a>hello</a></p>' -> '<a>hello</a>'
+					r => r.replace(/^<[^>]+>|<\/[^>]+>$/gm, '')
+				)
+			);
+		});
+
+		res.writeHead(200, {
+			'Content-Type': 'application/json'
+		});
+
+		let quote = tags[Math.floor((Math.random() * tags.length) + 1)];
+
+		res.end(JSON.stringify({quote: quote}));
+	});
+}
