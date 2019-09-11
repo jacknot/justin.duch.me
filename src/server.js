2a560cf08a6497ab638c0eca43ce8bc6ec87cdb7
Justin Duch <justin@justinduch.com>
2019-09-11 17:56:47 +1000

    add cors

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
+const cors = require('cors')({ origin:true });
 const { json } = require('body-parser');
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV !== 'production';
@@ -12,6 +13,7 @@ polka() // You can also use Express
 		json(),
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
+		cors,
 		sapper.middleware()
 	)
 	.listen(PORT, err => {
