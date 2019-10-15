0305af0ba122a9c7e510ca7fec9e590c26b9d8d9
Justin Duch <justin@justinduch.com>
2019-10-15 16:43:27 +1100

    about page changes

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -45,17 +45,27 @@
 </svelte:head>
 
 <h2>whoami</h2>
-<p>I am a person with opinions. Sometimes I publish those opinions to the internet. If you like or dislike my opinions you can email me about your own opinions here:</p>
+<p>I am a person with opinions. Sometimes I publish those opinions to the internet. Here are a few of my opinions so you know what you're getting into:</p>
+<ul>
+	<li>Garbage men and pick-up artists should change names.</li>
+	<li>Red pandas > normal pandas.</li>
+	<li>Stone Ocean is best JoJo part.</li>
+	<li>The bourgeoisie are destroying our planet.</li>
+</ul>
+<small><strong>Disclamer:</strong> One of these is actually a fact, not an opinion.</small>
+<p>If you like or dislike my opinions you can email me about your own opinions here:</p>
 <p><strong>Mail:</strong> justin@justinduch.com <a href="/publickey.txt">(PGP: 311DC918E44F90D5)</a></p>
 <p>It should go without saying that all opinions are my own and do not reflect those of anyone I work for.</p>
 
 <br>
 <h2>FAQ</h2>
-<p><strong>What Linux distro do you use?</strong> <br> Hannah Montana Linux.</p>
 <p><strong>How many mechanical keyboards do you have?</strong> <br> Five.</p>
 <p><strong>Cute boys or girls?</strong> <br> Both.</p>
-<p><strong>Where's the body?</strong> <br> Behind the windmill.</p>
+<p><strong><a href="/article/i_use_arch_btw" target="_blank">What Operating System do you use?</a></strong> <br> NodeJS.</p>
 <p><strong>How old are you?</strong> <br> {time} seconds.</p>
+<p><strong>Where were you on Sunday 22 November 2015?</strong> <br> At a friend's house.</p>
+<p><strong>Are these questions actually frequently asked?</strong> <br> Yes.</p>
+<p><strong>UwU?</strong> <br> NO!</p>
 
 <br>
 <h2>Subscribe</h2>
